d3dd8ad1470e57291a68b1cdb03db32c933db933
MedBouzidi-dev <mohamedcabouzidi@gmail.com>
2026-02-19 01:24:18 +0100

    ajouter des taches

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -12,6 +12,19 @@ use App\Repository\TacheRepository;
 
 final class TacheController extends AbstractController
 {
+#[Route('/taches/ajouter', name: 'app_ajout_tache')]
+public function nouveau(EntityManagerInterface $em): Response
+{
+    $tache = new Tache();
+    $tache->setTitre('Mon premier tache');
+    $tache->setDescription('Ceci est le contenu de mon premier article créé avec Doctrine.');
+    $tache->setDeteCreation(new \DateTime());
+
+    $em->persist($tache);
+    $em->flush();
+
+    return new Response("tache créé avec l'id : " . $tache->getId());
+}
 #[Route('/taches', name: 'app_taches')]
 public function index(TacheRepository $tacheRepository): Response
 {
