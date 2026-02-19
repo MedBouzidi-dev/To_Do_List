2b27530173d4e4275541900396c47f8019a22b4b
MedBouzidi-dev <mohamedcabouzidi@gmail.com>
2026-02-19 01:18:07 +0100

    affichage des tache depuis bd

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Entity\Tache;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\TacheRepository;
+
 final class TacheController extends AbstractController
 {
-    #[Route('/tache', name: 'app_tache')]
-    public function index(): Response
-    {
-        return $this->render('tache/index.html.twig', [
-            'controller_name' => 'TacheController',
-        ]);
-    }
+#[Route('/taches', name: 'app_taches')]
+public function index(TacheRepository $tacheRepository): Response
+{
+    $taches = $tacheRepository->findAll();
+
+    return $this->render('tache/index.html.twig', [
+        'taches' => $taches,
+    ]);
+}
+
 }
